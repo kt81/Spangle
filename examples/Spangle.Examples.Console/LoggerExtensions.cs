3cd27b35e6f843314a620397476d221bb41b1c12
kt81 <ktsh81@gmail.com>
2023-01-09 01:01:32 +0900

    exampleといいながら動作確認に使うやつ

---
+++ examples/Spangle.Examples.Console/LoggerExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Cysharp.Text;
+using Microsoft.Extensions.Logging;
+using ZLogger;
+
+namespace Spangle.Examples.Console;
+
+public static class LoggerExtensions
+{
+    public static void AddColorizedZLoggerConsole(this ILoggingBuilder builder, string concernedTarget)
+    {
+        builder.AddZLoggerConsole(configureEnableAnsiEscapeCode: true, configure: options =>
+        {
+            options.PrefixFormatter = (writer, info) =>
+            {
+                if (info.LogLevel == LogLevel.Error)
+                {
+                    ZString.Utf8Format(writer, "\u001b[31m[{0}][{1}] ", info.LogLevel, info.CategoryName);
+                }
+                else
+                {
+                    if (!info.CategoryName.StartsWith(concernedTarget))
+                    {
+                        ZString.Utf8Format(writer, "\u001b[38;5;08m[{0}][{1}] ", info.LogLevel, info.CategoryName);
+                    }
+                    else
+                    {
+                        ZString.Utf8Format(writer, "[{0}][{1}] ", info.LogLevel, info.CategoryName);
+                    }
+                }
+            };
+            options.SuffixFormatter = (writer, info) =>
+            {
+                if (info.LogLevel == LogLevel.Error || !info.CategoryName.StartsWith(concernedTarget))
+                {
+                    ZString.Utf8Format(writer, "\u001b[0m", "");
+                }
+            };
+        });
+    }
+}
---
+++ examples/Spangle.Examples.Console/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Buffers;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.Extensions.Logging;
+using Spangle.Examples.Console;
+using Spangle.Rtmp;
+using ZLogger;
+
+var listenEndPoint = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 1935);
+var listener = new TcpListener(listenEndPoint);
+var loggerFactory = LoggerFactory.Create(conf =>
+{
+    conf.AddFilter("Spangle", LogLevel.Trace)
+        .AddColorizedZLoggerConsole("Spangle");
+});
+var logger = loggerFactory.CreateLogger("Spangle.Examples.Console");
+
+listener.Start();
+logger.ZLogInformation("Starting to accept connection.");
+
+// Do NOT use this code in production!
+while (true)
+{
+    try
+    {
+        var tcpClient = await listener.AcceptTcpClientAsync();
+        _ = Task.Run(() => ProcessConnection(tcpClient, logger, loggerFactory) );
+    }
+    catch (Exception e)
+    {
+        logger.ZLogError("Error: {0}", e);
+    }
+}
+
+static async ValueTask ProcessConnection(TcpClient tcpClient, ILogger logger, ILoggerFactory loggerFactory)
+{
+    try
+    {
+        logger.ZLogInformation("Connection opened [{0}]", tcpClient.GetHashCode());
+        await using var stream = tcpClient.GetStream();
+        var rtmp = new RtmpReceiver(stream, loggerFactory);
+        await rtmp.BeginReadAsync();
+    }
+    catch (Exception e)
+    {
+        logger.ZLogError("Error: {0}", e);
+    }
+    finally
+    {
+        logger.ZLogInformation("Connection closed [{0}]", tcpClient.GetHashCode());
+    }
+}
