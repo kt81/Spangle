c59f4618d630fd236b0323583e16978bf363d4a6
kt81 <ktsh81@gmail.com>
2023-12-21 01:03:38 +0900

    preparing enhanced-rtmp

--- src/Spangle.Core/VideoCodec.cs
+++ src/Spangle.Core/VideoCodec.cs
@@ -3,13 +3,14 @@
 namespace Spangle;
 
 [SuppressMessage("ReSharper", "InconsistentNaming")]
-public enum VideoCodec
+public enum VideoCodec : uint
 {
-    H264 = 1,
-    H265 = 2,
-    H266 = 3,
+    // Internal FourCC
+    H264 = 'h' << 24 | '2' << 16 | '6' << 8 | '4',
+    H265 = 'h' << 24 | '2' << 16 | '6' << 8 | '5',
+    H266 = 'h' << 24 | '2' << 16 | '6' << 8 | '6',
 
-    VP8 = 11,
-    VP9 = 12,
-    AV1 = 13,
+    VP8 = 'v' << 24 | 'p' << 16 | '0' << 8 | '8',
+    VP9 = 'v' << 24 | 'p' << 16 | '0' << 8 | '9',
+    AV1 = 'a' << 24 | 'v' << 16 | '0' << 8 | '1',
 }
