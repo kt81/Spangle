6aa797c311e422f58c0be39ab02c537261002ece
kt81 <ktsh81@gmail.com>
2024-01-16 02:41:24 +0900

    connect spinner

--- src/Spangle.Core/Spinner/NALFileToAnnexBSpinner.cs
+++ src/Spangle.Core/Spinner/NALFileToAnnexBSpinner.cs
@@ -1,27 +1,47 @@
-﻿using System.IO.Pipelines;
+﻿using System.Buffers;
+using System.IO.Pipelines;
+using Microsoft.Extensions.Logging;
+using Spangle.Interop;
+using Spangle.Logging;
 
 namespace Spangle.Spinner;
 
 public class NALFileToAnnexBSpinner : ISpinner<INALFileFormatSpinnerIntakeAdapter, INALAnnexBSpinnerOutletAdapter>
 {
     private readonly INALFileFormatSpinnerIntakeAdapter _spinnerIntake;
-    private readonly INALAnnexBSpinnerOutletAdapter    _spinnerOutlet;
+    private readonly INALAnnexBSpinnerOutletAdapter     _spinnerOutlet;
+    private readonly CancellationToken                  _ct;
 
     private bool _hasSentAUD = false;
     private bool _hasSentPES = false;
 
-    public NALFileToAnnexBSpinner(INALFileFormatSpinnerIntakeAdapter spinnerIntake, INALAnnexBSpinnerOutletAdapter spinnerOutlet)
+    private static ILogger<NALFileToAnnexBSpinner> s_logger = SpangleLogManager.GetLogger<NALFileToAnnexBSpinner>();
+
+    public NALFileToAnnexBSpinner(INALFileFormatSpinnerIntakeAdapter spinnerIntake, INALAnnexBSpinnerOutletAdapter spinnerOutlet, CancellationToken ct)
     {
         _spinnerIntake = spinnerIntake;
         _spinnerOutlet = spinnerOutlet;
+        _ct = ct;
     }
 
-    public ValueTask SpinAsync()
+    public async ValueTask SpinAsync()
     {
         // TODO implement
-        // _input.VideoInput.
+        while (!_ct.IsCancellationRequested)
+        {
+            var result = await _spinnerIntake.VideoReader.ReadAsync(_ct);
+            var buff = result.Buffer;
+            while (buff.Length > 0)
+            {
+                _spinnerOutlet.VideoWriter.Write(buff.FirstSpan);
+                buff = buff.Slice(buff.FirstSpan.Length);
+            }
 
-        return ValueTask.CompletedTask;
+            _spinnerIntake.VideoReader.AdvanceTo(buff.End);
+            await _spinnerOutlet.VideoWriter.FlushAsync(_ct);
+            // TODO implement correctly
+        }
+
     }
 }
 
